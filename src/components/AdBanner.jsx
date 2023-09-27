e1db1d983c420787679a7d650cb661d22086f853
sanathsb99 <sanathkarthika@gmail.com>
2023-09-27 20:28:34 +0530

    commit

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -5,6 +5,7 @@ import AdPic from '../assets/ad-banner.png'
 const AdBanner = () => {
   return (
     <div className='ad-banner'>
+      {/* Ad Banner  */}
       <img className='ad-image' src={AdPic} alt='ad-banner'/>
     </div>
   )
